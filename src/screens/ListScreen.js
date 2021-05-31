e7d091d340c6f7fe17b83123e82c7abaa58b9db2
yusuf963 <mohammad963yusuf@gmail.com>
2021-06-01 00:35:27 +0100

    adding component

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,29 +1,39 @@
 import React from 'react'
 import {View, Text, StyleSheet, FlatList} from 'react-native'
 
-const ListScreen =()=>{
+const ListScreen =(props)=>{
+    console.log(props)
     const fruits =[
-        {name: 'oranges'},
-        {name: 'Banana'},
-        {name: 'Mango'},
-        {name: 'Grapes'}, 
-
+        {name: 'oranges', price: 1.99},
+        {name: 'Banana', price: 1.55},
+        {name: 'Mango', price: 0.99},
+        {name: 'Appel', price: 1.50}, 
+        {name: 'Carrot', price: 1.99}, 
+        {name: 'Pinapple', price: 1.25}, 
+        {name: 'onion', price: 1.56}, 
+        {name: 'Garlic', price: 1.30}, 
     ]
-    return<View style={style.listStyle}>
+
+    return<View >
         <Text>list of options</Text>
         <FlatList
+        // horizontal
+        showsHorizontalScrollIndicator = {false}
+        showsVerticalScrollIndicator = {false}
+         keyExtractor={fruits => fruits.name}
          data={fruits}
-          renderItem={({item,index})=>{
-              return <Text key={index}>{item.name}</Text>
+         renderItem={({item})=>{
+            return <Text style={styles.listStyle}>Name: {item.name} - Price: {item.price}</Text>
         }}
          />
     </View>
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     listStyle:{
+        marginVertical:50,
         fontSize:12,
-        color: 'blue'
+        color: 'green'
     }
 })
 
