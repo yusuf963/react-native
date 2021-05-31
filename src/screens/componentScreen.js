f86fbbdac1d7ae87cf4377ef92487e2ead5a8232
yusuf963 <mohammad963yusuf@gmail.com>
2021-05-31 23:32:38 +0100

    adding cmoenent to app.js

--- src/screens/componentScreen.js
+++ src/screens/componentScreen.js
@@ -1,13 +1,26 @@
 import React from 'react'
-import {Text, StyleSheet} from 'react-native'
+import {Text, StyleSheet, View} from 'react-native'
 
 const ComponentScreen =()=>{
-    return <Text style={styles.testStyle}>this is a compeonent screen </Text>
+    const test = <Text style={styles.secondText}>its text tag using variable</Text>
+    const msg = 'hi, it great seeing you'
+    return <View>
+        <Text style={styles.testStyle}>this is a compeonent screen </Text>
+        <Text style={styles.secondText}>Hi, its another test tag</Text>
+        {test}
+        <Text>{msg}</Text>
+        </View>
 }
 
 const styles = StyleSheet.create({
     testStyle:{
+        fontFamily:'sans-serif',
         fontSize: 50
+    },
+    secondText:{
+        fontFamily: 'helvetica',
+        fontSize: 20,
+        color: 'red'
     }
 })
 
